db5191cdbcbfdda98d74538ea735e2b852bd45cc
Giovanni Delsoto <gio.delsoto@gmail.com>
2025-01-05 10:24:44 -0300

    Atualizando .gitignore para ignorar arquivos e pastas específicas

--- backend/app/routes.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, render_template
-from backend.services.database_service import DatabaseService
-
-blueprint = Blueprint('routes', __name__)
-
-@blueprint.route('/')
-def index():
-    return render_template('index.html')
